b3c4e466645ed000a3059dc8c97fa53ccc988260
bryantt23 <bryantt23@DESKTOP-7I0G27J>
2018-06-25 04:48:52 -0700

    Initial commit

--- BanasWarriors/Program.cs
+++ BanasWarriors/Program.cs
@@ -1,15 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BanasWarriors
+﻿namespace BanasWarriors
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Warrior warrior1 = new Warrior("Steve");
+            Warrior warrior2 = new Warrior("Tony");
+
+            warrior1.Enemy = warrior2;
+            warrior2.Enemy = warrior1;
+
+            while (true)
+            {
+                warrior1.Attack();
+                if (!warrior2.IsAlive)
+                {
+                    break;
+                }
+                warrior2.Attack();
+                if (!warrior1.IsAlive)
+                {
+                    break;
+                }
+            }
         }
     }
 }
---
+++ BanasWarriors/Warrior.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BanasWarriors
+{
+    class Warrior
+    {
+        public int HP { get; set; }
+        public String Name { get; set; }
+        public Warrior Enemy { get; set; }
+        public Boolean IsAlive { get; set; }
+
+        public Warrior(String name)
+        {
+            this.Name = name;
+            this.HP = 100;
+            this.IsAlive = true;
+        }
+
+        public void Attack()
+        {
+            Random rnd = new Random();
+            int AttackPower = rnd.Next(10, 50);
+            this.Enemy.HP -= AttackPower;
+            if (this.Enemy.HP <= 0)
+            {
+                Console.WriteLine(this.Name + " has defeated " + this.Enemy.Name);
+                this.Enemy.IsAlive = false;
+            }
+            else
+            {
+                Console.WriteLine(this.Name + " attack power is " + AttackPower + ". ");
+                Console.WriteLine(this.Enemy.Name + " now has " + this.Enemy.HP + " HP");
+            }
+        }
+    }
+}
